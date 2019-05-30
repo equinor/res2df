f3e9b4a110e2ecd2d16dcd91c09852c8c44fd3f5
Håvard Berland <havb@equinor.com>
2019-05-30 20:29:36 +0200

    Add saturation function extracter

---
+++ ecl2df/satfunc2df.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Extract saturation function data (SWOF, SGOF, SWFN, ...)
+from an Eclipse deck as Pandas DataFrame
+
+"""
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import argparse
+import numpy as np
+import pandas as pd
+
+from .eclfiles import EclFiles
+
+KEYWORD_COLUMNS = {
+    "SWOF": ["SW", "KRW", "KROW", "PCOW"],
+    "SGOF": ["SG", "KRG", "KROG", "PCOG"],
+    "SWFN": ["SW", "KRW", "PCOW"],
+    "SOFN": ["SW", "KRO"],
+    "SGFN": ["SG", "KRG", "PCOG"],
+    "SOF3": ["SO", "KROW", "KROG"],
+    "SLGOF": ["SL", "KRG", "KROG", "PCOG"],
+}
+
+
+def deck2satfuncdf(deck):
+    """Extract the data in the saturation function keywords as a Pandas
+    DataFrame.
+
+    Data for all saturation functions are merged into one dataframe.
+    The two first columns in the dataframe are 'KEYWORD' (which can be
+    SWOF, SGOF, etc.), and then SATNUM which is an index counter from 1 and
+    onwards. Then follows the data for each individual keyword that
+    is found in the deck.
+
+    Return:
+        pd.DataFrame, columns 'SW', 'KRW', 'KROW', 'PC', ..
+    """
+    frames = []
+    for keyword in KEYWORD_COLUMNS.keys():
+        if keyword in deck:
+            satnum = 1
+            for deckrecord in deck[keyword]:
+                # All data for an entire SATNUM is returned in one list
+                data = np.array(deckrecord[0])
+                # Split up into the correct number of columns
+                column_count = len(KEYWORD_COLUMNS[keyword])
+                if len(data) % column_count:
+                    print("ERROR: Inconsistent data length or bug")
+                    return
+                satpoints = int(len(data) / column_count)
+                df = pd.DataFrame(columns=KEYWORD_COLUMNS[keyword],
+                        data=data.reshape(satpoints, column_count))
+                df["SATNUM"] = satnum
+                df["KEYWORD"] = keyword
+                df = df[['KEYWORD', 'SATNUM'] + KEYWORD_COLUMNS[keyword]]
+                satnum += 1
+                frames.append(df)
+
+    return pd.concat(frames, axis=0, sort=False)
+
+def parse_args():
+    """Parse sys.argv using argparse"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("DATAFILE", help="Name of Eclipse DATA file.")
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        help="Name of output csv file.",
+        default="satfuncs.csv",
+    )
+    return parser.parse_args()
+
+
+def main():
+    """Entry-point for module, for command line utility"""
+    args = parse_args()
+    eclfiles = EclFiles(args.DATAFILE)
+    if eclfiles:
+        deck = eclfiles.get_ecldeck()
+    satfunc_df = deck2satfuncdf(deck)
+    satfunc_df.to_csv(args.output, index=False)
+    print("Wrote to " + args.output)

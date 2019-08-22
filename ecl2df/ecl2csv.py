8fea5d7fd5d245d8a77f8f8e87e5b47ec266fbdb
Håvard Berland <havb@equinor.com>
2019-08-22 12:39:29 +0200

    Add support for faults in ecl2csv

--- ecl2df/ecl2csv.py
+++ ecl2df/ecl2csv.py
@@ -11,14 +11,14 @@ from __future__ import division
 
 import argparse
 
-from ecl2df import grid2df, nnc2df
+from ecl2df import grid2df, nnc2df, faults2df
 
 
 def main():
     """Entry-point"""
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
-    
+
     grid_parser = subparsers.add_parser("grid", help="Extract grid data")
     grid2df.fill_parser(grid_parser)
     grid_parser.set_defaults(func=grid2df.grid2df_main)
@@ -26,10 +26,14 @@ def main():
     nnc_parser = subparsers.add_parser("nnc", help="Extract nnc data")
     nnc2df.fill_parser(nnc_parser)
     nnc_parser.set_defaults(func=nnc2df.nnc2df_main)
-    
+
+    faults_parser = subparsers.add_parser("faults", help="Extract faults data")
+    faults2df.fill_parser(faults_parser)
+    faults_parser.set_defaults(func=faults2df.faults2df_main)
+
     args = parser.parse_args()
     args.func(args)
-    
+
 
 if __name__ == "__main__":
     main()

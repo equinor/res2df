a2b4641bec247ac145a74259cc2cc8e6775cf493
Håvard Berland <havb@equinor.com>
2019-10-16 14:38:17 +0200

    Add selected packages to __init__

--- ecl2df/__init__.py
+++ ecl2df/__init__.py
@@ -1,5 +1,16 @@
 from .eclfiles import EclFiles
 
+import grid2df
+import compdat2df
+import eclfiles
+import equil2df
+import faults2df
+import gruptree2df
+import nnc2df
+import satfunc2df
+import summary2df
+import wcon2df
+
 name = "ecl2df"
 
 from ._version import get_versions

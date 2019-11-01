f52dd2b91692589ed5c11243100a77ff6ec57bbb
Håvard Berland <havb@equinor.com>
2019-11-01 13:14:20 +0100

    Add features to rst2df
    * specify which vectors to include
    * Stack the dataframe with respect to restart dates

--- ecl2df/grid.py
+++ ecl2df/grid.py
@@ -96,19 +96,22 @@ def dates2rstindices(eclfiles, dates):
     return (rstindices, chosendates)
 
 
-def rst2df(eclfiles, date, dateinheaders=False, datestacked=False):
+def rst2df(eclfiles, date, vectors=None, dateinheaders=False, datestacked=False):
     """Return a dataframe with dynamic data from the restart file
     for each cell, at a particular date.
 
     Args:
         eclfiles: EclFiles object
+
         date: datetime.date or list of datetime.date, must
             correspond to an existing date. If list, it
             forces dateinheaders to be True.
             Can also be string, then the mnenomics
             'first', 'last', 'all', are supported, or ISO
             date formats.
-        dateinheaders: boolean on whether the date should
+         vectors (str or list): List of vectors to include,
+            glob-style wildcards supported
+         dateinheaders: boolean on whether the date should
             be added to the column headers. Instead of
             SGAS as a column header, you get SGAS@YYYY-MM-DD.
         datestacked (bool): Default is false. If true, a column
@@ -116,6 +119,12 @@ def rst2df(eclfiles, date, dateinheaders=False, datestacked=False):
             dates will be added in a stacked manner. Implies
             dateinheaders False.
     """
+    if not vectors:
+        vectors = "*"  # This will include everything
+    if not isinstance(vectors, list):
+        vectors = [vectors]
+    logging.info("Extracting vectors {} from RST file".format(str(vectors)))
+
     # First task is to determine the restart index to extract
     # data for:
     (rstindices, chosendates) = dates2rstindices(eclfiles, date)
@@ -128,7 +137,9 @@ def rst2df(eclfiles, date, dateinheaders=False, datestacked=False):
     activecells = eclfiles.get_egrid().getNumActive()
     rstvectors = []
     for vec in eclfiles.get_rstfile().headers:
-        if vec[1] == activecells:
+        if vec[1] == activecells and any(
+            [fnmatch.fnmatch(vec[0], key) for key in vectors]
+        ):
             rstvectors.append(vec[0])
     rstvectors = list(set(rstvectors))  # Make unique list
     # Note that all of these might not exist at all timesteps.
@@ -165,13 +176,18 @@ def rst2df(eclfiles, date, dateinheaders=False, datestacked=False):
         )
 
         # For users convenience:
-        if "SWAT" in rst_df and "SGAS" in rst_df and "SOIL" not in rst_df:
+        if (
+            "SWAT" in rst_df
+            and "SGAS" in rst_df
+            and "SOIL" not in rst_df
+            and any([fnmatch.fnmatch("SOIL", key) for key in vectors])
+        ):
             rst_df["SOIL"] = 1 - rst_df["SWAT"] - rst_df["SGAS"]
 
         # Tag the column names if requested, or if multiple rst indices
         # are asked for
         datestr = chosendates[rstindices.index(rstindex)].isoformat()
-        if dateinheaders or len(rstindices) > 1:
+        if dateinheaders or len(rstindices) > 1 and not datestacked:
             rst_df.columns = [colname + "@" + datestr for colname in rst_df.columns]
 
         rst_dfs[datestr] = rst_df

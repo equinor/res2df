b27eac6615c90b6496f6b601f51effc2275135a3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-03 12:12:09 +0000

    Deploy equinor/ecl2df to github.com/equinor/ecl2df.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.2.dev9+g5edb527',
+    VERSION: '0.5.2.dev10+gd85f1fc',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

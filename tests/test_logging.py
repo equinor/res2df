7139be40ad6f14f251bcf81030e2fd099499f7ff
Anders Fredrik Kiær <akia@equinor.com>
2020-11-25 10:14:41 +0100

    Add verbose logging test

---
+++ tests/test_logging.py
@@ -0,0 +1,28 @@
+import logging
+import argparse
+
+import pytest
+
+from ecl2df.grid import grid_main
+from test_grid import DATAFILE
+
+
+@pytest.mark.parametrize("verbose", [False, True])
+def test_grid_logging(caplog, tmp_path, verbose):
+
+    args = argparse.Namespace(
+        verbose=verbose,
+        DATAFILE=DATAFILE,
+        vectors="*",
+        rstdates="",
+        dropconstants=False,
+        stackdates=False,
+        output=tmp_path / "eclgrid.csv",
+    )
+
+    grid_main(args)
+
+    if verbose:
+        assert caplog.records
+    else:
+        assert caplog.records == []

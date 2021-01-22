da22ada05ed8a692fa4be60f9190ff2ac6d81bfb
ecl2df-github-action <ecl2df-github-action>
2021-01-22 13:09:13 +0000

    Update Github Pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.2.dev8+g4828541',
+    VERSION: '0.9.2.dev9+g5213f4e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

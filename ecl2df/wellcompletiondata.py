092d01506351edae9e77e14acb47142af18779fd
Øyvind Lind-Johansen <olind@equinor.com>
2022-05-21 14:03:50 +0200

    Implemented merged_compdat and wellconnstatus function and fixed pylint issues

--- ecl2df/wellcompletiondata.py
+++ ecl2df/wellcompletiondata.py
@@ -1,23 +1,25 @@
 """Aggregates completion data from layer to zone"""
 
 import argparse
-import logging
 from pathlib import Path
-from typing import Any, List
 
 import pandas as pd
 
-from ecl2df import compdat, getLogger_ecl2csv
+from ecl2df import compdat, getLogger_ecl2csv, wellconnstatus
 from ecl2df.eclfiles import EclFiles
 
 from .common import write_dframe_stdout_file
 
-logger = logging.getLogger(__name__)
 
-
-def df(eclfiles: EclFiles, zonemap_filename: str) -> pd.DataFrame:
+def df(
+    eclfiles: EclFiles, zonemap_filename: str, use_wellconnstatus: bool
+) -> pd.DataFrame:
     """Info"""
     compdat_df = compdat.df(eclfiles, zonemap_filename=zonemap_filename)
+
+    if use_wellconnstatus:
+        wellconnstatus_df = wellconnstatus.df(eclfiles)
+        compdat_df = _merge_compdat_and_connstatus(compdat_df, wellconnstatus_df)
 
     records = []
     for (well, zone, date), group_df in compdat_df.groupby(["WELL", "ZONE", "DATE"]):
@@ -33,6 +35,45 @@ def df(eclfiles: EclFiles, zonemap_filename: str) -> pd.DataFrame:
             }
         )
     return pd.DataFrame(records)
+
+
+def _merge_compdat_and_connstatus(
+    compdat_df: pd.DataFrame, wellconnstatus_df: pd.DataFrame
+) -> pd.DataFrame:
+    """This function merges the compdat data with the well connection status data
+    (extracted from the CPI summary data). The connection status data will be used
+    for wells where it exists. The KH will be merged from the compdat. For wells
+    that are not in the connection status data, the compdat data will be used as it is.
+
+    This approach is fast, but a couple of things should be noted:
+    * in the connection status data, a connection is not set to SHUT before it has been
+    OPEN. In the compdat data, some times all connections are first defined and the
+    opened later.
+    * any connections that are in compdat but not in connections status will be ignored
+    (e.g. connections that are always shut)
+    * there is no logic to handle KH changing with time for the same connection (this
+    can easily be added using apply in pandas, but it is very rare and slows down the
+    function significantly)
+    * if connection status is missing for a realization, but compdat exists, compdat
+    will also be ignored.
+    """
+    match_on = ["REAL", "WELL", "I", "J", "K1"]
+    dframe = pd.merge(
+        wellconnstatus_df, compdat_df[match_on + ["KH"]], on=match_on, how="left"
+    )
+
+    # There will often be several rows (with different OP/SH) matching in compdat.
+    # Only the first is kept
+    dframe.drop_duplicates(subset=["DATE"] + match_on, keep="first", inplace=True)
+
+    # Concat from compdat the wells that are not in well connection status
+    dframe = pd.concat(
+        [dframe, compdat_df[~compdat_df.WELL.isin(dframe["WELL"].unique())]]
+    )
+
+    dframe = dframe.reset_index(drop=True)
+    dframe["KH"] = dframe["KH"].fillna(0)
+    return dframe
 
 
 def fill_parser(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
@@ -79,7 +120,7 @@ def wellcompletiondata_main(args):
     if not Path(args.zonemap).is_file():
         raise FileNotFoundError(f"{args.zonemap} does not exists.")
 
-    wellcompletiondata_df = df(eclfiles, args.zonemap)
+    wellcompletiondata_df = df(eclfiles, args.zonemap, args.use_wellconnstatus)
     write_dframe_stdout_file(
         wellcompletiondata_df, args.output, index=False, caller_logger=logger
     )

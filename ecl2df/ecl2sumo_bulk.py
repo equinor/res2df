9d2e43884be6d896aa2a3aadc76883359cc1b7b2
Daniel Berge Sollien <daniel.sollien@gmail.com>
2023-04-26 07:38:32 +0200

    Remove global dict as default argument
    flagged as dangerous option by pylint

--- ecl2df/ecl2sumo_bulk.py
+++ ecl2df/ecl2sumo_bulk.py
@@ -12,45 +12,65 @@ from ecl2df.rft import export_w_metadata as rft
 from ecl2df.satfunc import export_w_metadata as satfunc
 from ecl2df.summary import export_w_metadata as summary
 
-options = {
-    "initvectors": None, #List[str]
-    "keywords": None, #List[str] x3
-    "fipname": "FIPNUM", #str
-    "vectors": "*", #List[str]
-    "rstdates": "", #str
-    "stackdates": False, #bool x2
-    "dropconstants": False, #bool
-    "arrow": False, #bool x2
-    "coords": False, #bool  
-    "pillars": False, #bool
-    "region": "", #str
-    "rstdates": "", #str
-    "soilcutoff": 0.5, #float
-    "sgascutoff": 0.5, #float
-    "swatcutoff": 0.5, #float
-    "group": False, #bool
-    "wellname": None, #str
-    "date": None, #str
-    "time_index": "raw", #str
+standard_options = {
+    "initvectors": None,  # List[str]
+    "keywords": None,  # List[str] x3
+    "fipname": "FIPNUM",  # str
+    "vectors": "*",  # List[str]
+    "stackdates": False,  # bool x2
+    "dropconstants": False,  # bool
+    "arrow": False,  # bool x2
+    "coords": False,  # bool
+    "pillars": False,  # bool
+    "region": "",  # str
+    "rstdates": "",  # str
+    "soilcutoff": 0.5,  # float
+    "sgascutoff": 0.5,  # float
+    "swatcutoff": 0.5,  # float
+    "group": False,  # bool
+    "wellname": None,  # str
+    "date": None,  # str
+    "time_index": "raw",  # str
     "column_keys": None,
-    "start_date": "", #str
-    "end_date": "", #str
-    "params": False, #bool
-    "paramfile": None, #str
-    "include_restart": False, #bool
+    "start_date": "",  # str
+    "end_date": "",  # str
+    "params": False,  # bool
+    "paramfile": None,  # str
+    "include_restart": False,  # bool
 }
 
-def bulk_upload(eclpath, metadata_path, include:List = None, options = options):
+
+def bulk_upload(eclpath, metadata_path, include: List = None, options=None):
     """Bulk uploads every module to sumo with metadata
 
-        eclpath (str): path to eclipse datafile
-        metadata_path (str): path to metadata file
-        include (List): list of submodules to include. Defaults to None which includes all
+    eclpath (str): path to eclipse datafile
+    metadata_path (str): path to metadata file
+    include (List): list of submodules to include. Defaults to None which includes all
     """
-    subfunc_list = [compdat, equil, faults, fipreports, grid, nnc, pillars, pvt, rft, satfunc, summary]
+    if options is None:
+        options = standard_options
+
+    subfunc_list = [
+        compdat,
+        equil,
+        faults,
+        fipreports,
+        grid,
+        nnc,
+        pillars,
+        pvt,
+        rft,
+        satfunc,
+        summary,
+    ]
 
     for subfunc in subfunc_list:
         if include is None or subfunc.__name__ in include:
             sig_items = signature(subfunc).parameters.items()
-            filtered_options = {name: options[name] for name, param in sig_items if param.kind is not Parameter.empty and name not in {"eclpath", "metadata_path"}}
+            filtered_options = {
+                name: options[name]
+                for name, param in sig_items
+                if param.kind is not Parameter.empty
+                and name not in {"eclpath", "metadata_path"}
+            }
             subfunc(eclpath, metadata_path, **filtered_options)

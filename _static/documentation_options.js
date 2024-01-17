bd7cc0fbb218fd22ae39d6fba6cf1788cd2ddc96
res2df-github-action <res2df-github-action>
2024-01-17 13:45:14 +0000

    Update Github Pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.1.dev7+gcc784dee',
+    VERSION: '1.0.1.dev8+gfdf25e42',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

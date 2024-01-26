af5b68b054db9aa1c0a119f469d6ec78060607eb
Håvard Berland <havb@equinor.com>
2024-01-26 15:31:05 +0100

    Add regression test for extracting summary data in RESTART
    Runs the Eclipse simulator on a mocked deck, and asserts
    that it is possible to extract full history when requested
    and the filenames are within certain limits imposed by Eclipse.

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,3 +9,20 @@ import res2df
 def path_to_res2df():
     """Path to installed res2df module"""
     return Path(res2df.__file__).parent
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--run-eclipse-simulator",
+        action="store_true",
+        default=False,
+        help="Include tests that run the Eclipse reservoir simulator",
+    )
+
+
+def pytest_collection_modifyitems(config, items):
+    for item in items:
+        if item.get_closest_marker("requires_eclipse") and not config.getoption(
+            "--run-eclipse-simulator"
+        ):
+            item.add_marker(pytest.mark.skip("Requires eclipse"))

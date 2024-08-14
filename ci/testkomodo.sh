148263f5eddb1ff710aa45e49a02424a54d599cc
larsevj <levje@equinor.com>
2024-08-14 11:40:32 +0200

    Remove unused function from testkomodo.sh

--- ci/testkomodo.sh
+++ ci/testkomodo.sh
@@ -2,10 +2,6 @@ install_test_dependencies () {
   pip install -r test_requirements.txt
 }
 
-install_package () {
-  pip install .[tests,docs]
-}
-
 start_tests () {
   pytest --run-eclipse-simulator
 }

7647a97ab2e0f1f4eb4ae9905ec67413ad447741
res2df-github-action <res2df-github-action>
2026-07-29 12:12:46 +0000

    Update Github Pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.3.16.dev4+gf36e3b427',
+    VERSION: '1.3.16.dev5+gd857ce168',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
